29c8165f61936ce4d42c863c5e54992891e2b443
Luis Carlos <luiscarlos_ja@outlook.com>
2024-02-11 19:57:03 -0600

    Add initial project files and configurations

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App(): JSX.Element {
+  return <h1>CRUD using Redux Toolkt & React.js with TypeAScript</h1>
+}
+
+export default App
